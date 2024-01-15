2c6d3b22bcda16fa0d9d2f0365cd15354b77cf12
Gonçalo Cruz Moreira <cruzmoreira1990@gmail.com>
2024-01-15 05:30:24 +0000

    IT WORKS

--- app/src/main/java/ipca/project/climamate/MainActivity.kt
+++ app/src/main/java/ipca/project/climamate/MainActivity.kt
@@ -14,7 +14,6 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var weatherViewModel: WeatherViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
